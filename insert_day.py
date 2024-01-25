b0c95b727ee403983787231ace1c974336dbdcb2
dpetz <Dirk.Petzoldt@gmail.com>
2024-01-25 13:39:49 +0100

    explorations updated

--- insert_day.py
+++ insert_day.py
@@ -7,6 +7,17 @@ import hashlib # hashlib.algorithms_available
 # md5 is available on every platform and is 128-bit short
 # see https://en.wikipedia.org/wiki/List_of_hash_functions
 
+
+import numpy as np
+import pandas as pd
+from sales_util import read_liquor_csv
+
+df = read_liquor_csv('day_1')
+
+date = df['Date'][0]
+
+print(date)
+
 db_path = "data/db/liquor.sqlite"
-con = sqlite3.connect(db_path) #":memory:"
-cur = con.cursor()
+#con = sqlite3.connect(db_path) #":memory:"
+#cur = con.cursor()
